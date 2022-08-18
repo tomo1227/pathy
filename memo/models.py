78aa92d16e358ff427b41be3a518602e1bc422cd
Tomoki <tomo.soccer1227@gmail.com>
2022-08-18 23:31:12 +0900

    First commit

---
+++ memo/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+
+class User(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+class Article(models.Model):
+    title = models.CharField(max_length=30)
+    article_text = models.CharField(max_length=1000)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
