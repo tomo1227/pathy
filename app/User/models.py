820951bf9cc7c073be0acad355168b93d37afa4e
Shota0616 <ju.oh0616@gamil.com>
2022-08-28 21:31:11 +0900

    Add Userモデルの実装

--- app/User/models.py
+++ app/User/models.py
@@ -1,77 +1,72 @@
 from django.db import models
-from django.contrib.auth.base_user import AbstractBaseUser
-from django.contrib.auth.validators import UnicodeUsernameValidator
-from django.core.mail import send_mail
-from django.utils import timezone
-from django.utils.translation import gettext_lazy as _
-from django.contrib.auth.models import PermissionsMixin, UserManager
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
 
-
-class MyUser(AbstractBaseUser, PermissionsMixin):
-    """
-    An abstract base class implementing a fully featured User model with
-    admin-compliant permissions.
-    Username and password are required. Other fields are optional.
-    """
-
-    username_validator = UnicodeUsernameValidator()
-
-    username = models.CharField(
-        _("username"),
-        max_length=150,
-        unique=True,
-        help_text=_(
-            "Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only."
-        ),
-        validators=[username_validator],
-        error_messages={
-            "unique": _("A user with that username already exists."),
-        },
-    )
-    first_name = models.CharField(_("first name"), max_length=150, blank=True)
-    last_name = models.CharField(_("last name"), max_length=150, blank=True)
-    email = models.EmailField(_("email address"), blank=True)
-    is_staff = models.BooleanField(
-        _("staff status"),
-        default=False,
-        help_text=_("Designates whether the user can log into this admin site."),
-    )
-    is_active = models.BooleanField(
-        _("active"),
-        default=True,
-        help_text=_(
-            "Designates whether this user should be treated as active. "
-            "Unselect this instead of deleting accounts."
-        ),
-    )
-    date_joined = models.DateTimeField(_("date joined"), default=timezone.now)
-
-    objects = UserManager()
-
-    EMAIL_FIELD = "email"
-    USERNAME_FIELD = "username"
-    REQUIRED_FIELDS = ["email"]
-
-    class Meta:
-        verbose_name = _("user")
-        verbose_name_plural = _("users")
-        #abstract = True
-
-    def clean(self):
-        super().clean()
-        self.email = self.__class__.objects.normalize_email(self.email)
-
-    def get_full_name(self):
+# cliでユーザー作成するときのclass
+class MyUserManager(BaseUserManager):
+    # 一般ユーザーの作成
+    def create_user(self, email, username, password=None):
         """
-        Return the first_name plus the last_name, with a space in between.
+        Creates and saves a User with the given email, date of
+        birth and password.
         """
-        full_name = "%s %s" % (self.first_name, self.last_name)
-        return full_name.strip()
+        if not email:
+            raise ValueError('Users must have an email address')
 
-    def get_short_name(self):
-        """Return the short name for the user."""
-        return self.first_name
+        user = self.model(
+            email=self.normalize_email(email),
+            username=username,
+        )
 
-    def email_user(self, subject, message, from_email=None, **kwargs):
-        """Send an email to this user."""
-        send_mail(subject, message, from_email, [self.email], **kwargs)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    # 管理ユーザーの作成
+    def create_superuser(self, email, username, password=None):
+        """
+        Creates and saves a superuser with the given email, date of
+        birth and password.
+        """
+        user = self.create_user(
+            email,
+            username=username,
+            password=password,
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
+class MyUser(AbstractBaseUser):
+    email = models.EmailField(verbose_name='メールアドレス',max_length=255,unique=True)
+    username = models.CharField(verbose_name='ユーザー名',max_length=50,unique=True,blank=True) # 空文字を許容
+    password = models.CharField(max_length=128, verbose_name='パスワード')
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    objects = MyUserManager()
+
+    # 認証に使用するfield
+    USERNAME_FIELD = 'email'
+
+    EMAIL_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app `app_label`?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    @property
+    def is_staff(self):
+        "Is the user a member of staff?"
+        # Simplest possible answer: All admins are staff
+        return self.is_admin
