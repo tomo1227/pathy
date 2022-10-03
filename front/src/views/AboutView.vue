e753232d442a09af6a3dc941d8fb11c064bbe20b
Tomoki <tomo.soccer1227@gmail.com>
2022-10-04 01:08:09 +0900

    ADD #17 marked追加

--- front/src/views/AboutView.vue
+++ front/src/views/AboutView.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <HelloMarkdown />
   </div>
 </template>
+
+<script>
+// @ is an alias to /src
+import HelloMarkdown from "@/components/HelloMarkdown.vue";
+
+export default {
+  name: "AboutView",
+  components: {
+    HelloMarkdown,
+  },
+};
+</script>
