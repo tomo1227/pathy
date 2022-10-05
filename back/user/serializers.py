0e92c5ad78a883e3b42524bfcd6c2b3d80808042
Shota0616 <ju.oh0616@gamil.com>
2022-10-05 23:23:32 +0900

    Userからuserに変更

---
+++ back/user/serializers.py
@@ -0,0 +1,8 @@
+'''
+from django.contrib.auth.hashers import make_password #追加
+
+from rest_framework import serializers
+from djoser.serializers import UserSerializer
+
+from .models import MyUser
+'''
