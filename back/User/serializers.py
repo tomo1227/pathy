fd22609a66ecc38664b1b1bdd82dd61a7eefd92b
Shota0616 <ju.oh0616@gamil.com>
2022-10-05 23:35:47 +0900

    fix

--- back/User/serializers.py
+++
@@ -1,8 +0,0 @@
-'''
-from django.contrib.auth.hashers import make_password #追加
-
-from rest_framework import serializers
-from djoser.serializers import UserSerializer
-
-from .models import MyUser
-'''
