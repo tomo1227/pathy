7e04c9040d9a735b7c797be1344188db2a81990c
Tomoki <tomo.soccer1227@gmail.com>
2022-11-04 19:13:39 +0900

    投稿する記事のuserをuser_mailで特定

--- back/apps/memo/serializer.py
+++ back/apps/memo/serializer.py
@@ -11,8 +11,8 @@ logger = logging.getLogger(__name__)
 
 class ArticleSerializer(serializers.ModelSerializer):
     author = UserSerializer(read_only=True)
-    user_id = serializers.PrimaryKeyRelatedField(
-        queryset=MyUser.objects.all(), write_only=True
+    user_email = serializers.SlugRelatedField(
+        queryset=MyUser.objects.all(), write_only=True,slug_field='email'
     )
 
     class Meta:
@@ -21,10 +21,10 @@ class ArticleSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         logger.debug(validated_data)
-        validated_data["author"] = validated_data.get("user_id", None)
+        validated_data["author"] = validated_data.get("user_email", None)
         if validated_data["author"] is None:
             raise serializers.ValidationError("User not found.")
 
-        del validated_data["user_id"]
+        del validated_data["user_email"]
 
         return Article.objects.create(**validated_data)
